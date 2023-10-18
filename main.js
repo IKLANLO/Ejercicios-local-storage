bc4d8a31f53f65ec73f954ddd7b6fd51948db6cd
IKLANLO <ilandaberea@yahoo.es>
2023-10-18 20:36:22 +0200

    version con extra

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
-const btnSubmit = document.getElementById('submit')
-const btnReset = document.getElementById('reset')
-const txtName = document.getElementById('name')
-const txtEmail = document.getElementById('mail')
-const txtMessage = document.getElementById('message')
+// const btnSubmit = document.getElementById('submit')
+// const btnReset = document.getElementById('reset')
+// const txtName = document.getElementById('name')
+// const txtEmail = document.getElementById('mail')
+// const txtMessage = document.getElementById('message')
+
+let btnSubmit = document.getElementById('submit')
+let btnReset = document.getElementById('reset')
+let txtName = document.getElementById('name')
+let txtEmail = document.getElementById('mail')
+let txtMessage = document.getElementById('message')
+let arr = []
 
 function guardarDatos(e) {
     e.preventDefault()
-    localStorage.setItem('user', JSON.stringify({
+
+    arr.push({
         name: txtName.value,
         email: txtEmail.value,
         message: txtMessage.value
-    }))
-    const userData = JSON.parse(localStorage.getItem('user'))
-    console.log('Datos de usuario',userData)
+    })
+
+    localStorage.setItem('user',JSON.stringify(arr))
+
+    let userData = JSON.parse(localStorage.getItem('user'))
+    console.log('Datos de usuario\n',userData)
 } 
 
 btnSubmit.addEventListener('click', guardarDatos)
